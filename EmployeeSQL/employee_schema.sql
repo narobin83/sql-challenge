80d2c15cac79fce619427976b8696c00147f4a69
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-01 23:14:03 -0400

    employee_schema.sql

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "departments" (
+CREATE TABLE departments (
     dept_no VARCHAR(10)   NOT NULL,
     dept_name VARCHAR(30)   NOT NULL,
 	PRIMARY KEY (dept_no)
@@ -6,7 +6,7 @@ CREATE TABLE "departments" (
 
 SELECT * FROM departments
 
-CREATE TABLE "employees" (
+CREATE TABLE employees (
     emp_no INT   NOT NULL,
     birth_date DATE   NOT NULL,
     first_name VARCHAR(20)   NOT NULL,
@@ -17,3 +17,14 @@ CREATE TABLE "employees" (
 );
 
 SELECT * FROM employees
+
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp
