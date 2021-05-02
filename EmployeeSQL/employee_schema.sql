f0cd85f514e092b3f09014e3ce4dea3ca17c840f
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-01 22:55:21 -0400

    employee_schema.sql

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -4,4 +4,16 @@ CREATE TABLE "departments" (
 	PRIMARY KEY (dept_no)
 );
 
-SELECT * from departments
+SELECT * FROM departments
+
+CREATE TABLE "employees" (
+    emp_no INT   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(20)   NOT NULL,
+    last_name VARCHAR(20)   NOT NULL,
+    gender VARCHAR(2)   NOT NULL,
+    hire_date DATE   NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM employees
