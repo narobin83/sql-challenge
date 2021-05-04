bc1c48fd69fb5a778ee3c54e2d4c07d594c97ae6
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-03 21:48:24 -0400

    queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -40,5 +40,16 @@ order by emp_no;
 SELECT * FROM employees
 WHERE first_name = 'Hercules' AND last_name like 'B%';
 
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
 
 
